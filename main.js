dd6dedb41614748786361fe592324f50dee25908
Aman <taalaibekov003@gmail.com>
2021-11-28 12:28:24 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let numb = 4.6;
+
+console.log(4/0);
+
+console.log('string'*9);
+
+const person = "Alex";
+
+const bool = false;
+
+let und;
+
+console.log(und);
+
+const obj = {
+    name: "Aman",
+    age: 25,
+    isMaried: false
+};
+
+console.log(obj["name"]);
+
+const result = confirm("Sure?");
+console.log(result);
+
+const res = prompt("Your age:");
+console.log(typeof(res));
+
+a=5;
+console.log(a);
